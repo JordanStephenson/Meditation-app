b250a9be19356f57ae3ff380ffbb16eb225ddde3
Jordan Stephenson <jordan_535@hotmail.com>
2020-02-10 00:29:07 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const app = () => {
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
+
+  //Sounds
+  const sounds = document.querySelectorAll(".sound-select button");
+  //Time display
+  const timeDisplay = document.querySelector(".time-display");
+  const timeSelect = document.querySelectorAll(".time-select button");
+  //Get lenght of outline
+  const outlineLength = outline.getTotalLength();
+  //Duration
+  let fakeDuration = 600;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  //Pick Sounds
+  sounds.forEach(sound => {
+    sound.addEventListener("click", function() {
+      song.src = this.getAttribute("data-sound");
+      video.src = this.getAttribute("data-video");
+      checkPlaying(song);
+    });
+  });
+
+  //Play sound
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+  //Select sound
+  timeSelect.forEach(option => {
+    option.addEventListener("click", function() {
+      fakeDuration = this.getAttribute("data-time");
+      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+        fakeDuration % 60
+      )}`;
+    });
+  });
+
+  //Function to stop and play sounds
+  const checkPlaying = song => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "/meditation-app-master/svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "/meditation-app-master/svg/play.svg";
+    }
+  };
+
+  //Circle animation prep
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    //Circle animation
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+    //Animate text
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+
+    if (currentTime >= fakeDuration) {
+      song.pause();
+      video.pause();
+      song.currentTime = 0;
+      play.src = "/meditation-app-master/svg/play.svg";
+    }
+  };
+};
+
+app();
